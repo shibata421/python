c3f2b60580896842855c06d1abe039e71a2cc064
shibata421 <shibata.421@gmail.com>
2022-07-20 14:23:58 -0300

    todo-v4

--- curso-python/poo/todo_v4.py
+++ curso-python/poo/todo_v4.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class Projeto:
@@ -10,8 +10,8 @@ class Projeto:
     def __iter__(self):
         return self.tarefas.__iter__()
 
-    def add(self, descricao):
-        self.tarefas.append(Tarefa(descricao))
+    def add(self, descricao, vencimento=None):
+        self.tarefas.append(Tarefa(descricao, vencimento))
 
     def pendentes(self):
         return [tarefa
@@ -28,21 +28,33 @@ class Projeto:
 
 
 class Tarefa:
-    def __init__(self, descricao):
+    def __init__(self, descricao, vencimento=None):
         self.descricao = descricao
         self.feito = False
         self.criacao = datetime.now()
+        self.vencimento = vencimento
 
     def concluir(self):
         self.feito = True
 
     def __str__(self):
-        return '- ' + self.descricao + (' (Concluída)' if self.feito else '')
+        status = []
+
+        if self.feito:
+            status.append('(Concluída)')
+        elif self.vencimento:
+            if datetime.now() > self.vencimento:
+                status.append('(Vencida)')
+            else:
+                dias = (self.vencimento - datetime.now()).days
+                status.append(f'(Vence em {dias} dias)')
+
+        return f'{self.descricao} ' + ' '.join(status)
 
 
 def main():
     casa = Projeto('Tarefas de casa')
-    casa.add('Passar roupa')
+    casa.add('Passar roupa', datetime.now())
     casa.add('Lavar prato')
     print(casa)
 
@@ -54,7 +66,7 @@ def main():
     mercado = Projeto('Compras no mercado')
     mercado.add('Frutas secas')
     mercado.add('Carne')
-    mercado.add('Tomate')
+    mercado.add('Tomate', datetime.now() + timedelta(days=3, seconds=1))
     print(mercado)
 
     comprar_carne = mercado.procurar('Carne')
