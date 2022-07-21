4a91ab2b3ba8adfccb6c76bff1da5a083d05d5b7
Fernando Shibata <shibata.421@gmail.com>
2022-07-20 22:12:17 -0300

    multipla heranca

---
+++ curso-python/poo_avancada/multipla.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+class Animal:
+    @property
+    def capacidades(self):
+        return ('dormir', 'comer', 'beber')
+
+
+class Homem(Animal):
+    @property
+    def capacidades(self):
+        return super().capacidades + ('amar', 'falar', 'estudar')
+
+
+class Aranha(Animal):
+    @property
+    def capacidades(self):
+        return super().capacidades + ('fazer teia', 'andar pelas paredes')
+
+
+class Spiderman(Aranha, Homem):
+    @property
+    def capacidades(self):
+        return super().capacidades + ('bater em bandidos', 'atirar teias entre prédios')
+
+
+if __name__ == "__main__":
+    john = Homem()
+    print(f'John: {john.capacidades}')
+
+    aranha = Aranha()
+    print(f'Aranha: {aranha.capacidades}')
+
+    peter = Spiderman()
+    print(f'Peter: {peter.capacidades}')
