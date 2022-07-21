37e9afe957f68e3e96e8a4c563e69d09109cc5e1
shibata421 <shibata.421@gmail.com>
2022-07-21 12:40:15 -0300

    Inclui fatorial de 0

--- curso-python/programacao_funcional/recursividade.py
+++ curso-python/programacao_funcional/recursividade.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def fatorial(n):
-    if n == 1:
+    if n in (0, 1):
         return 1
     return n * fatorial(n - 1)
 
 
 if __name__ == "__main__":
+    print(f'0! = {fatorial(1)}')
     print(f'1! = {fatorial(1)}')
     print(f'2! = {fatorial(2)}')
     print(f'3! = {fatorial(3)}')
