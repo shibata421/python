16d70d4b50d57ebc1078bb5f540b1491b1e5f4ba
shibata421 <shibata.421@gmail.com>
2022-07-21 20:34:44 -0300

    Funcoes imutabilidade 2

---
+++ curso-python/programacao_funcional/funcoes_imutabilidade_v2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from functools import reduce
+from operator import add
+
+valores = (30, 10, 25, 70, 100, 94)
+
+print(sorted(valores))
+print(valores)
+
+print(min(valores))
+print(max(valores))
+print(sum(valores))
+print(reduce(add, valores))
+print(reversed(valores))
+print(list(reversed(valores)))
