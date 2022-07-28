d9695eddab5e01e890877407f317b82fa38a2e41
Fernando Shibata <shibata.421@gmail.com>
2022-07-27 22:42:20 -0300

    le contatos

---
+++ curso-python/banco_dados/ler_contatos.py
@@ -0,0 +1,13 @@
+from db import nova_conexao
+from mysql.connector.errors import ProgrammingError
+
+with nova_conexao() as conexao:
+    try:
+        cursor = conexao.cursor()
+        cursor.execute('SELECT * FROM contatos')
+        contatos = cursor.fetchall()
+    except ProgrammingError as e:
+        print(f'Erro: {e.msg}')
+    else:
+        [print(f'{contato[2]:2d} - {contato[0]:6s} Telefone: {contato[1]}')
+         for contato in contatos]
